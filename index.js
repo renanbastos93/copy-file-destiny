daaa113ed9b34d2d12c9e68a02f1fd26bf7facc3
Renan Bastos <renanbastos.tec@gmail.com>
2017-05-25 02:07:40 -0400

    [*] best file, but less replace with array

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
 const fs = require('fs');
-// const copyFileDestiny = {};
+var api = {};
 
-function init(param){
-  _readyFile = fs.readFileSync(__dirname+'/'+param[0], 'utf8');
-  changeVariables('teste', 'renan', _readyFile, param[1]);
+var copy = (old, actual, oldFile, newFile) => {
+  if(api.exists(oldFile)){
+    var ready = api.ready(oldFile);
+    ready = (Array.isArray(old)) ? old.map((item, index) => {
+      return (Array.isArray(actual) && actual.length == old.length) ?
+      actual.map((act, index, arr) => {
+        return api.changeVariablesVector(item, act, ready);
+      }) : ready
+    }) : api.changeVariablesSingle(old, actual, ready);
+  }
+  var data = (Array.isArray(ready)) ? ready.toString() : ready;
+  return {stream: api.create(newFile), data: data};
 };
 
-function changeVariables(oldVar, newVar, ready, file){
-  ready = ready.split(oldVar).join(newVar);
-  fs.createWriteStream(file, 'utf8').write(ready);
-}
-
-init(['newTeste.js', 'endTeste.js']);
-// init({0:'newTeste.js', 1:'endTeste.js'});
-
-function copyFileDestiny(oldVar, newVar, ready, file){
-   if(Array.isArray(oldVar) && Array.isArray(newVar)){
-      return this.changeVariables(arguments);
-   }
-   if(Array.isArray(oldVar) || Array.isArray(newVar)){
-     return {error: new Error('Arguments of lenght different')};
-   }
-
+api.exists = function(file){
+  return fs.existsSync('./'+file);
 };
 
-copyFileDestiny.prototype = copyFileDestiny;
-
-copyFileDestiny.prototype.changeVariables = function(oldVar, newVar, ready, file){
-  ready = ready.split(oldVar).join(newVar);
+api.changeVariablesSingle = function(old, actual, ready){
+  return ready.split(old).join(actual);
 };
+
+api.changeVariablesVector = function(old, actual, ready){
+  return ready.split(/\;/).map(item => {
+    return item.replace(old, actual)
+  });
+};
+
+api.ready = function(file){
+    return fs.readFileSync(__dirname+'/'+file, 'utf8');
+};
+
+api.create = function(file){
+  // return fs.createReadStream(file).pipe(ready);
+  return fs.createWriteStream(file, 'utf8');
+};
+
+var a = copy(['SERVPUB', 'init'], ['renan2', 'end'], 'newTeste.js', 'muchParams.js');
+var b = copy('SERVPUB', 'renan', 'newTeste.js', 'normaly.js');
+a.stream.write(a.data)
+b.stream.write(b.data)
