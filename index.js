39825b9ae5d1b248ea3e22970de4aa2f479db757
renanbastos93 <renanbastos.tec@gmail.com>
2017-08-01 03:38:47 +0000

    [*] better project fixed create new file only file

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 const fs = require('fs');
-var api = {};
+const copyFileDestiny = {};
 
-var copy = (vars, oldFile, newFile) => {
-  var data = '';
-  if(api.exists(oldFile)){
-    var ready = api.ready(oldFile);
-    data = api.changeVariables(vars, ready);
-  }
-  return {stream: api.create(newFile), data: data};
-};
-
-api.exists = function(file){
+copyFileDestiny.exists = function(file){
   return fs.existsSync('./'+file);
 };
 
-api.changeVariables = function(vars, ready){
+copyFileDestiny.changeVariables = function(vars, ready){
 	for(v in vars){
 		ready = ready.replace(RegExp(v, 'gi'), vars[v]);
 	};
 	return ready;
 };
 
-api.ready = function(file){
+copyFileDestiny.ready = function(file){
     return fs.readFileSync(__dirname+'/'+file, 'utf8');
 };
 
-api.create = function(file){
+copyFileDestiny.create = function(file){
   return fs.createWriteStream(file, 'utf8');
 };
 
-// var a = copy({teste: 'renan', init: 'inicio', fim: 'end'},'newTeste.js', 'muchParams.js');
-// var b = copy({'teste': 'renan'}, 'newTeste.js', 'normaly.js');
-// a.stream.write(a.data)
-// b.stream.write(b.data)
+module.exports = function(vars, oldFile, newFile){
+  var data = '';
+  if(copyFileDestiny.exists(oldFile)){
+    var ready = copyFileDestiny.ready(oldFile);
+    data = copyFileDestiny.changeVariables(vars, ready);
+  }
+  if(data != ('' || null || undefined)){
+    return copyFileDestiny.create(newFile).write(data);
+  }
+  return false;
+};
