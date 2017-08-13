520b885940f6a65cead34bee8f7073ba8ace2aed
renanbastos93 <renanbastos.tec@gmail.com>
2017-08-13 05:08:37 -0300

    [+] start CLI

---
+++ cli.js
@@ -0,0 +1,8 @@
+var program = require('commander');
+
+program
+    .version(require('../package.json').version)
+    .usage('[command] [options] [directory]')
+    .option('-h, --help', 'I\'ll help you the copy your files for with other variables or it changed.')
+    .parse(process.argv)
+    ;
